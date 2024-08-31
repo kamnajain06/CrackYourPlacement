c4c708639da2cb2e6db6e4e86718581fba29807e
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-08-31 08:11:20 +0530

    Time: 14 ms (9.29%), Space: 20.1 MB (5.19%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -14,17 +14,17 @@ public:
     bool flag = false;
     int count = 0;
     void postOrder(TreeNode* root, int targetSum){
-        count++;
+        // count++;
         if(root == NULL){
             return;
         }
-        if(root->left == NULL && root->right == NULL && targetSum == root->val && count > 1) flag=1;
+        if(root->left == NULL && root->right == NULL && targetSum == root->val) flag=1;
         postOrder(root->left, targetSum-root->val);
         postOrder(root->right, targetSum-root->val);
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == NULL) return 0;
-        if(root->left == NULL && root->right == NULL && root->val == targetSum) return true;
+        // if(root->left == NULL && root->right == NULL && root->val == targetSum) return true;
         postOrder(root,targetSum);
         return flag;
     }
