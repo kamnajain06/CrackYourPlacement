c15f1a99243902f580b59335cf5bcc1c5e3117e8
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-09-14 22:42:02 +0530

    Update 0114-flatten-binary-tree-to-linked-list.cpp

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,25 +11,60 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> v;
-    void preorder(TreeNode* root){
-        if(root == NULL) return;
-        v.push_back(root);
-        preorder(root->left);
-        preorder(root->right);
-    }
+
+    // In-space - Recursive Solution
+    TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
-        int i = 1;
-        preorder(root);
-        for(auto it:v){
-            cout<<it->val<<endl;
-        }
-        while( i < v.size()){
-            root->left = NULL;
-            root->right = v[i];
-            root = root->right;
-            i++;
-        }
+        if(root == NULL) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
         return;
+    }
+
+// In-space - Iterative Solution
+// void flatten(TreeNode* root) {
+//         stack<TreeNode*> st;
+//         if(root == NULL) return;
+//         st.push(root);
+
+//         while(!st.empty()){
+//             TreeNode* curr = st.top();
+//             st.pop();
+
+//             if(curr->right != NULL) st.push(curr->right);
+//             if(curr->left != NULL) st.push(curr->left);
+
+//             if(!st.empty()){
+//                 curr->right = st.top();  
+//             }
+//             curr->left = NULL;
+//         }
+//         return;
+//     }
+
+    //extra space 
+    // vector<TreeNode*> v;
+    // void preorder(TreeNode* root){
+    //     if(root == NULL) return;
+    //     v.push_back(root);
+    //     preorder(root->left);
+    //     preorder(root->right);
+    // }
+    // void flatten(TreeNode* root) {
+    //     int i = 1;
+    //     preorder(root);
+    //     for(auto it:v){
+    //         cout<<it->val<<endl;
+    //     }
+    //     while( i < v.size()){
+    //         root->left = NULL;
+    //         root->right = v[i];
+    //         root = root->right;
+    //         i++;
+    //     }
+    //     return;
     }
 };
