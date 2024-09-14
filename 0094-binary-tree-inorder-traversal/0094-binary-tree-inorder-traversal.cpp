4db06ff42593375004005cfbf27e2013ee5005b8
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-09-14 18:31:54 +0530

    Update 0094-binary-tree-inorder-traversal.cpp

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,22 +12,50 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> v;
-        stack<TreeNode*> st;
-        
-        TreeNode* node = root;
-        while(true){
-            if(node != NULL){
-                st.push(node);
-                node = node->left;
+
+        // Using Morris Traversal - threaded binary tree
+        vector<int> ans;
+        TreeNode* curr = root;
+
+        while(curr != NULL){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
             }else{
-                if(st.empty()) break;
-                node = st.top();
-                v.push_back(node->val);
-                st.pop();
-                node = node->right;
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    curr = curr->left;
+                }else{
+                    prev->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
         }
-        return v;
+        return ans;
+
+        // Using Stack
+        
+        // vector<int> v;
+        // stack<TreeNode*> st;
+        
+        // TreeNode* node = root;
+        // while(true){
+        //     if(node != NULL){
+        //         st.push(node);
+        //         node = node->left;
+        //     }else{
+        //         if(st.empty()) break;
+        //         node = st.top();
+        //         v.push_back(node->val);
+        //         st.pop();
+        //         node = node->right;
+        //     }
+        // }
+        // return v;
     }
 };
