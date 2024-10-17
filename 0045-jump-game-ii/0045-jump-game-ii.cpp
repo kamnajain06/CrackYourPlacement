585c1508753ee9f171fcc4671e2397d8fb0c6bfe
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-10-18 00:29:30 +0530

    Time: 0 ms (100.00%), Space: 19.2 MB (78.95%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int cnt = 0;
+        int l = 0;
+        int r = 0;
+        while(r < nums.size()-1){
+            int far = 0;
+            for(int i=l ; i<=r; i++){
+                far = max(far,i+nums[i]);
+            }
+            l = r+1;
+            r = far;
+            cnt++;
+        }
+        return cnt;
+    }
+};
