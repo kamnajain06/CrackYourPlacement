1c23bdaf8f04173eadcad8b2c823df282af3aa57
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-11-15 02:53:09 +0530

    Time: 5 ms (50.47%), Space: 17.9 MB (42.44%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0);
+        queue<int> q;
+        vector<int> topo;
+        unordered_map<int,vector<int>> mpp;
+        for(int i=0; i<prerequisites.size(); i++){
+            indegree[prerequisites[i][0]] += 1;
+            mpp[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        while(!q.empty()){
+            int val = q.front();
+            q.pop();
+            topo.push_back(val);
+            for(auto it:mpp[val]){
+                if(indegree[it] != 0){
+                    indegree[it] -= 1;
+                    if(indegree[it] == 0) q.push(it);
+                }
+            }
+        }
+        vector<int> nullify;
+        if(topo.size() != numCourses) return nullify;
+        return topo;
+    }
+};
