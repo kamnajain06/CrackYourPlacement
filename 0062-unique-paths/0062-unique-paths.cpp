74b673eadd23f2ccd5050ed508531cb3a8395898
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-10 04:04:35 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (15.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int dfs(int i, int j, int m , int n, vector<vector<int>> &dp){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int right = dfs(i, j-1, m, n, dp);
-        int down = dfs(i-1, j, m, n, dp);
-        return dp[i][j] = right + down;
-    }
+    // int dfs(int i, int j, int m , int n, vector<vector<int>> &dp){
+    //     if(i == 0 && j == 0) return 1;
+    //     if(i < 0 || j < 0) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int right = dfs(i, j-1, m, n, dp);
+    //     int down = dfs(i-1, j, m, n, dp);
+    //     return dp[i][j] = right + down;
+    // }
     int uniquePaths(int m, int n) {
         // Memoization
         // vector<vector<int>> dp(m, vector<int> (n, -1));
