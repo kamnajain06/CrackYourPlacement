642910b46289a76168f8fe8f066ea221953a2b4f
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-09 20:11:20 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (9.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,8 +8,13 @@ public:
         return dp[n] = max(nums[n] + fun(n+2, sum , nums, dp), fun(n+1, sum, nums, dp));
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, -1);
-        int cnt = fun(0, 0, nums, dp);
-        return cnt;
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int pick = i-2 >= 0 ? nums[i] + dp[i-2] : nums[i];
+            dp[i] = max(pick, dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
