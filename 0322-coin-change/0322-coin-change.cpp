df915a7491acc620037aa637291675036a7ce6ee
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-13 02:17:00 +0530

    Time: 20 ms (75.31%), Space: 17.8 MB (57.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int fun(int amount, vector<int> &coins, int n, vector<int> &dp){
-        if(amount == 0) return 0;
+//     int fun(int amount, vector<int> &coins, int n, vector<int> &dp){
+//         if(amount == 0) return 0;
         
-        if(dp[amount] != -1) return dp[amount];
+//         if(dp[amount] != -1) return dp[amount];
 
-        int mini = 1e8;
-        for(int i=0; i<n; i++){
-            int pick = coins[i] <= amount ? 1 + fun(amount-coins[i], coins, n, dp) : 1e8;
-            mini = min(mini, pick);
-        }
+//         int mini = 1e8;
+//         for(int i=0; i<n; i++){
+//             int pick = coins[i] <= amount ? 1 + fun(amount-coins[i], coins, n, dp) : 1e8;
+//             mini = min(mini, pick);
+//         }
         
-        return dp[amount] = mini;
-    }
+//         return dp[amount] = mini;
+//     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1, -1);
-        int res = fun(amount, coins, n, dp);
-        return res == 1e8 ? -1 : res;
+        // vector<int> dp(amount+1, -1);
+        // int res = fun(amount, coins, n, dp);
+        // return res == 1e8 ? -1 : res;
+        
+        // TABULATION
+        vector<int> dp(amount+1, 0);
+        for(int it = 1; it<= amount; it++){
+            int mini = 1e8;
+            for(int i=0; i<n; i++){
+                int pick = coins[i] <= it ? 1 + dp[it-coins[i]] : 1e8;
+                mini = min(mini, pick);
+            }
+            dp[it] = mini;
+        }
+        return dp[amount] == 1e8 ? -1 : dp[amount];
     }
 };
