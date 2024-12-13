05f83930ace2cc48fba9f9269257dec03dd5062a
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-14 00:39:56 +0530

    Time: 38 ms (20.33%), Space: 28.4 MB (29.51%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int fun(int ind, int amount, vector<int> &coins, int n, vector<vector<int>> &dp){
+        if(amount == 0) return 1;
+        if(ind >= n || amount < 0) return 0;
+
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int pick = fun(ind, amount - coins[ind], coins, n, dp);
+        int skip = fun(ind+1, amount, coins, n, dp);
+
+        return dp[ind][amount] = pick + skip;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        // MEMOIZATION
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        return fun(0, amount, coins, n, dp);
+
+        // // TABULATION
+        // vector<vector<long long>> dp(n, vector<long long> (amount+1, 0));
+        // for(int row = 0; row<n; row++){
+        //     dp[row][0] = 1;
+        // }
+        // for(int row=0; row<n; row++){
+        //     for(int sum=1; sum<= amount; sum++){
+        //         int pick = sum - coins[row] >=0 ? dp[row][sum - coins[row]] : 0;
+        //         int skip = row-1 >=0 ? dp[row-1][sum] : 0;
+        //         dp[row][sum] = pick + skip;
+        //     }
+        // }
+        // return dp[n-1][amount];
+    }
+};
