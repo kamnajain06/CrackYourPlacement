4547b69085185f779c74d94f03dc5de0ba9477e5
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-17 14:29:34 +0530

    Time: 0 ms (100.00%), Space: 20.1 MB (59.03%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,25 +2,42 @@ class Solution {
 public:
     int minDeletions(string s) {
         int n = s.length();
-        unordered_map<char, int> mpp;
+        vector<int> alpha(26,0);
         for(int i=0; i<n ; i++){
-            mpp[s[i]]++;
+            alpha[abs('a'-s[i])]++;
         }
+        sort(alpha.begin(), alpha.end());
         int ans = 0;
-        unordered_map<char, int> mpp1;
-        for(auto it: mpp){
-            int val = it.second;
-            if(mpp1.find(val) == mpp1.end()) mpp1[it.second] = it.first;
+
+        for(int i=24; i>=0; i--){
+            if(alpha[i] == 0) break;
+            else if(alpha[i+1] == 0){
+                ans += alpha[i];
+                alpha[i] = 0;
+            }
+            else if(alpha[i] <= alpha[i+1]-1) continue;
             else{
-                while(mpp1.find(val) != mpp1.end()){
-                    val--;
+                while(alpha[i] > 0 && alpha[i] > alpha[i+1]-1){
                     ans++;
+                    alpha[i]--;
                 }
-                if(val > 0){
-                    mpp1[val] = it.first;
-                }
-            }   
+            }
         }
+        
+        // unordered_map<char, int> mpp1;
+        // for(auto it: mpp){
+        //     int val = it.second;
+        //     if(mpp1.find(val) == mpp1.end()) mpp1[it.second] = it.first;
+        //     else{
+        //         while(mpp1.find(val) != mpp1.end()){
+        //             val--;
+        //             ans++;
+        //         }
+        //         if(val > 0){
+        //             mpp1[val] = it.first;
+        //         }
+        //     }   
+        // }
         return ans;
     }
 };
