8cc9e82cfe1831df24afae5267938e9b10a603f0
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-20 00:51:02 +0530

    Time: 6 ms (63.35%), Space: 12.9 MB (50.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -15,8 +15,6 @@ public:
     int minDistance(string word1, string word2) {
         int n1 = word1.length();
         int n2 = word2.length();
-        if(n2 == 0 && n1 != 0) return n1;
-        else if (n1 == 0 && n2 != 0) return n2;
         vector<vector<int>> dp(n1, vector<int> (n2, -1));
         return fun(n1-1, n2-1, word1, word2, dp);
     }
