168a7ea80e84eac574251e8015a36d9b70ab669d
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2024-12-20 01:16:03 +0530

    Time: 7 ms (55.57%), Space: 12.4 MB (82.49%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -18,22 +18,40 @@ public:
         // vector<vector<int>> dp(n1, vector<int> (n2, -1));
         // return fun(n1-1, n2-1, word1, word2, dp);
 
-        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
-        for(int row = 1; row<= n1; row++){
-            dp[row][0] = row;
-        }
+        // vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
+        // for(int row = 1; row<= n1; row++){
+        //     dp[row][0] = row;
+        // }
+        // for(int col=1; col<= n2; col++){
+        //     dp[0][col] = col;
+        // }
+        // for(int i=1; i<=n1; i++){
+        //     for(int j = 1; j<=n2; j++){
+        //         if(word1[i-1] == word2[j-1]){
+        //              dp[i][j] = dp[i-1][j-1];
+        //         }else{
+        //             dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+        //         }
+        //     }
+        // }
+        // return dp[n1][n2];
+
+        vector<int> dp(n2+1, 0);
         for(int col=1; col<= n2; col++){
-            dp[0][col] = col;
+            dp[col] = col;
         }
         for(int i=1; i<=n1; i++){
+            vector<int> temp(n2+1, 0);
+            temp[0] = i;
             for(int j = 1; j<=n2; j++){
                 if(word1[i-1] == word2[j-1]){
-                     dp[i][j] = dp[i-1][j-1];
+                     temp[j] = dp[j-1];
                 }else{
-                    dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+                    temp[j] = 1 + min(temp[j-1], min(dp[j], dp[j-1]));
                 }
             }
+            dp = temp;
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
