2e75db7560bbfed841f62f7662b4814efed00980
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-03-19 16:30:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        // Your code here
+        int num1 = n >> k;
+        return num1 & 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main() {
+    int t;
+    cin >> t; // taking testcases
+    while (t--) {
+        long long n;
+        cin >> n; // input n
+        int k;
+        cin >> k; // bit number k
+        Solution obj;
+        if (obj.checkKthBit(n, k))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
