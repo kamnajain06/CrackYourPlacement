4b07983418bf47415d7b2298e2b258a5c29eb7e1
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-03-19 12:23:34 +0530

    Time: 5 ms (69.45%), Space: 140.1 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
-        set<int> mpp;
-        for(int i=0; i<n; i++){
+        // set<int> mpp;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] == 0){
+        //         mpp.insert(i);
+        //     }
+        // }
+        // if(mpp.empty()) return 0;
+        // int cnt = 0;
+        // if(mpp.size() < 2) return -1;
+        int i = 0, cnt = 0;
+        while(i < n-2){
             if(nums[i] == 0){
-                mpp.insert(i);
+                nums[i] ^= 1;
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
+                cnt++;
             }
+            i++;
+            
         }
-        if(mpp.empty()) return 0;
-        int cnt = 0;
-        if(mpp.size() < 2) return -1;
-        while(!mpp.empty()){
-            int i = *mpp.begin();
-            if(i + 3 > n) return -1;
-            for(int j = i; j < i+3 && j < n; j++){
-                if(nums[j] == 0){
-                    mpp.erase(j);
-                }else{
-                    mpp.insert(j);
-                }
-                nums[j] ^= 1;
-            }
-            cnt++;
-        }
-        return cnt;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum == n) return cnt;
+        return -1;
     }
 };
