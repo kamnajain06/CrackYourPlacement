453ef333413a67be3e86238b6b79a6d15939c261
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-03-19 16:36:37 +0530

    Time: 0 ms (100%), Space: 8 MB (19.78%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 0 || n < 0) return false;
-        while(n>1){
-            if(n%2 != 0) return false;
-            n /= 2;
-        }
+        if(n < 1) return false;
+        if(n & (n-1)) return false;
         return true;
     }
 };
