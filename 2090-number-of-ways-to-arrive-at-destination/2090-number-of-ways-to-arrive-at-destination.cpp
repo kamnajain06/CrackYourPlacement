1c7e28c64e133955da950cf0b5134df419722e00
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-03-23 19:16:50 +0530

    Time: 19 ms (19.54%), Space: 37.3 MB (50.25%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        unordered_map<int, vector< pair<int,int>>> mpp;
-        for(auto it:roads){
-            mpp[it[0]].push_back({it[1],it[2]});
-            mpp[it[1]].push_back({it[0],it[2]});
-
+        unordered_map<int, vector<pair<int, int>>> adjList;
+        for (auto it : roads) {
+            adjList[it[0]].push_back({it[1], it[2]});
+            adjList[it[1]].push_back({it[0], it[2]});
         }
-        priority_queue<pair<long long, int> , vector<pair<long long, int>>, greater<pair<long long, int>>> q;
-        vector<long long> dist(n, LLONG_MAX), ways(n,0) ;
-        q.push({0,0});
-        dist[0] = 0;
+        vector<long long> distance(n, LLONG_MAX), ways(n, 0);
+        priority_queue<pair<long long , int>, vector<pair<long long , int>>, greater<pair<long long , int>>> pq;
+        pq.push({0,0});
+        distance[0] = 0;
         ways[0] = 1;
-        vector<int> extra;
-        int mod = (int) 1e9 + 7;
-        while(!q.empty()){
-            auto val = q.top();
-            int node = val.second;
-            long long distance = val.first;
-            q.pop();
-            for(auto it:mpp[node]){
-                if(distance+it.second == dist[it.first]){
-                    ways[it.first] = (ways[it.first] + ways[node])%(mod);
+        long long mod = 1e9 + 7;
+        while(!pq.empty()){
+            auto it = pq.top();
+            int node = it.second;
+            long long dist = it.first;
+            pq.pop();
+            
+            for(auto it: adjList[node]){
+                if(it.second + dist == distance[it.first]){
+                    ways[it.first] +=( ways[node])%mod;
                 }
-                if(distance+it.second < dist[it.first]){
-                    dist[it.first] = distance+it.second;
+                if(it.second + dist < distance[it.first]){
+                    distance[it.first] = dist + it.second;
                     ways[it.first] = ways[node];
-                    q.push({distance+it.second,it.first});
+                    pq.push({distance[it.first], it.first});
                 }
             }
         }
-        
         return ways[n-1]%mod;
     }
 };
