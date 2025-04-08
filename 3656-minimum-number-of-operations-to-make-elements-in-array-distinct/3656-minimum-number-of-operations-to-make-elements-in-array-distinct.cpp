fe07baf0f3d582c84393d39878269a0231b38f1e
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-04-08 16:46:46 +0530

    Time: 0 ms (100%), Space: 27.8 MB (43.34%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int ans = 0;
-        int n = nums.size();
-        unordered_map<int, int> freqMpp, remMpp;
-        for (int i = 0; i < n; i++) {
-            freqMpp[nums[i]]++;
-        }
-        for (auto it : freqMpp)
-            if(it.second > 1){
-                remMpp[it.first] = it.second;
+       //try iterating from last
+       int n = nums.size();
+       unordered_map<int,int> freq;
+       for(int i=n-1; i>=0; i--){
+            if(freq.find(nums[i]) == freq.end()) freq[nums[i]]++;
+            else{
+                return ((i+1) % 3) == 0 ? (i+1)/3 : ((i+1)/3) + 1;
             }
-
-        int left = 0;
-        while (left < n) {
-            if (remMpp.empty())
-                return ans;
-            else ans++;
-            for (int i = 0; i < 3; i++) {
-                if (left + i < n) {
-                    if (remMpp.find(nums[left + i]) != remMpp.end()) {
-                        remMpp[nums[left + i]]--;
-                        if (remMpp[nums[left + i]] <= 1) {
-                            remMpp.erase(nums[left + i]);
-                        }
-                    }
-                } else
-                    return ans;
-            }
-            
-            left = left + 3;
-        }
-        return ans;
+       } 
+       return 0;
     }
 };
