722145a2e2e592e04e2a885f601ee6fb93589854
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-04-08 18:55:18 +0530

    Time: 89 ms (74.48%), Space: 16.4 MB (76.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,61 +1,62 @@
 class Solution {
 public:
-    // bool fun(int ind, int target, vector<int>& nums, vector<vector<int>> &dp){
-    //     if(target == 0 && ind > 0) return true;
-    //     if(ind < 0) return false;
+    // bool fun(int i, int n, vector<int>& nums, int target,
+    //          vector<vector<int>>& dp) {
+    //     if (i >= n) {
+    //         return target == 0;
+    //     }
+    //     if (dp[i][target] != -1)
+    //         return dp[i][target];
+    //     bool pick = nums[i] <= target
+    //                     ? fun(i + 1, n, nums, target - nums[i], dp)
+    //                     : false;
+    //     bool skip = fun(i + 1, n, nums, target, dp);
 
-    //     if(dp[ind][target] != -1) return dp[ind][target];
-
-    //     int pick = nums[ind] <= target ? fun(ind-1, target-nums[ind], nums, dp) : false;
-    //     int skip = fun(ind-1, target, nums, dp);
-    //     return dp[ind][target] = pick | skip; 
+    //     return dp[i][target] = pick | skip;
     // }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
-        for(int i=0; i<n; i++){
-            sum += nums[i];
-        }
-        if(sum%2 != 0) return false;
+        for (auto it : nums)
+            sum += it;
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
 
-        int target = sum/2;
+        // vector<vector<int>> dp(n , vector<int> (target+1, -1));
+        // return fun(0, n, nums, target, dp);
 
-        // MEMOIZATION
-        // vector<vector<int>> dp(n, vector<int> (target+1, -1));
-        // return fun(n-1, target, nums, dp);
-
-        // // TABULATION
-        // vector<vector<int>> dp(n, vector<int> (target+1, 0));
+        // vector<vector<int>> dp(n, vector<int>(target + 1, false));
         // for(int i=0; i<n; i++){
         //     dp[i][0] = true;
         // }
-        // if(nums[0] <= target){
-        //     dp[0][nums[0]] = true;
-        // }
-        // for(int i=1; i<n; i++){
-        //     for(int j=0; j<=target; j++){
-        //         int pick = nums[i] <= j ? dp[i-1][j-nums[i]]  : false;
-        //         int skip = dp[i-1][j];
-        //         dp[i][j] = pick | skip; 
+        // if(nums[i] <= target) dp[i][target] = true;
+        // for (int i = n-1; i >= 0; i--) {
+        //     for (int j = target; j >= 0; j--) {
+        //         bool pick = nums[i] <= j
+        //                         ? dp[i + 1][j - nums[i]]
+        //                         : false;
+        //         bool skip = dp[i + 1][j];
+
+        //         dp[i][j] = pick | skip;
         //     }
         // }
-        // return dp[n-1][target];
 
-        // SPACE OPT
-        vector<int> dp(target+1, 0);
+        vector<bool> dp(target + 1, false);
         dp[0] = true;
 
-        if(nums[0] <= target){
-            dp[nums[0]] = true;
-        }
+        if(nums[0] <= target) dp[nums[0]] = true;
 
         for(int i=1; i<n; i++){
-            vector<int> temp(target+1, 0);
+            vector<bool> temp(target+1, false);
             temp[0] = true;
-            for(int j=0; j<=target; j++){
-                int pick = nums[i] <= j ? dp[j-nums[i]]  : false;
-                int skip = dp[j];
-                temp[j] = pick | skip; 
+            for (int j = 0; j <= target; j++) {
+                bool pick = nums[i] <= j
+                                ? dp[j - nums[i]]
+                                : false;
+                bool skip = dp[j];
+
+                temp[j] = pick | skip;
             }
             dp = temp;
         }
