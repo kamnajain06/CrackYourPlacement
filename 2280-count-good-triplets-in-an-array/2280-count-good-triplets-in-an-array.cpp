c9cca48b345e42e72d24caf96786b0be64401ea5
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-04-17 03:29:11 +0530

    Time: 165 ms (23.36%), Space: 114.3 MB (26.98%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.cpp
@@ -1,49 +1,49 @@
-class FenwickTree {
-private:
-    vector<int> tree;
-
-public:
-    FenwickTree(int size) : tree(size + 1, 0) {}
-
-    void update(int index, int delta) {
-        index++;
-        while (index < tree.size()) {
-            tree[index] += delta;
-            index += index & -index;
-        }
-    }
-
-    int query(int index) {
-        index++;
-        int res = 0;
-        while (index > 0) {
-            res += tree[index];
-            index -= index & -index;
-        }
-        return res;
-    }
-};
-
 class Solution {
+private:
+    vector<int> segTree;
 public:
+
+    int query(int start, int end, int i, int l, int r){
+        if(r < start || l > end) return 0;
+        else if(l >= start && r <= end) return segTree[i];
+        int mid = l + (r-l)/2;
+        return segTree[i] = query(start, end, 2*i+1, l, mid) + query(start, end, 2*i+2, mid + 1, r);
+    }
+    void queryUpdate(int idx, int i, int l, int r){
+        if(l == r){
+            segTree[i] = 1;
+            return;
+        }
+        int mid = l + (r-l)/2;
+        if(idx <= mid){
+            queryUpdate(idx, 2*i+1, l, mid);
+        }else{
+            queryUpdate(idx, 2*i+2, mid+1, r);
+        }
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+        return;
+    }
     long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        vector<int> pos2(n), reversedIndexMapping(n);
-        for (int i = 0; i < n; i++) {
-            pos2[nums2[i]] = i;
+        segTree.resize(4*n, 0);
+        unordered_map<int,int> nums2Idx;
+
+        for(int i=0; i<n; i++){
+            nums2Idx[nums2[i]] = i;
         }
-        for (int i = 0; i < n; i++) {
-            reversedIndexMapping[pos2[nums1[i]]] = i;
+        long long ans = 0;
+        queryUpdate(nums2Idx[nums1[0]], 0, 0, n-1);
+        for(int i=1; i<n; i++){
+            int idx = nums2Idx[nums1[i]];  //idx of nums1[i] in nums2
+            long long leftCommonElements = query(0, idx, 0, 0, n-1);
+            long long leftUncommonElements = i - leftCommonElements;
+            long long rightElementsNums2 = n - idx - 1;
+            long long rightCommonElements = rightElementsNums2 - leftUncommonElements;
+            ans += (leftCommonElements * rightCommonElements);
+
+            queryUpdate(idx, 0, 0, n-1);
         }
-        FenwickTree tree(n);
-        long long res = 0;
-        for (int value = 0; value < n; value++) {
-            int pos = reversedIndexMapping[value];
-            int left = tree.query(pos);
-            tree.update(pos, 1);
-            int right = (n - 1 - pos) - (value - left);
-            res += (long long)left * right;
-        }
-        return res;
+
+        return ans;
     }
 };
