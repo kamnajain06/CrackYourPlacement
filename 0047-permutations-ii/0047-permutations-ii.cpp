8329267a71dac8da125829bd9e3e11cea9105075
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-10 19:08:54 +0530

    Time: 1044 ms (5%), Space: 12.1 MB (59.14%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rec(int n , vector<int> &nums, vector<int> &used, vector<int> &temp, set<vector<int>> &st){
+        for(int i=0; i<n; i++){
+            if(!used[i]){
+                temp.push_back(nums[i]);
+                used[i] = 1;
+                if(temp.size() == n) st.insert(temp);
+                cout<<st.size()<<endl;
+                rec(n, nums, used, temp, st);
+                temp.pop_back();
+                used[i] = 0;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> st;
+        vector<int> temp, used(n,0);
+        rec(n, nums, used, temp, st);
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
