fc84ae05297a062ad0817129ec7f877e09553e1c
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-11 00:57:45 +0530

    Time: 0 ms (100%), Space: 18.7 MB (52.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -22,11 +22,19 @@ public:
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        for(int i=0; i<m; i++){
-            if(matrix[0][0] == 0) matrix[i][0] = 0;
+        // for(int i=0; i<m; i++){
+        //     if(matrix[0][0] == 0) matrix[i][0] = 0;
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(row == 0) matrix[0][i] = 0;
+        // }
+        if(matrix[0][0] == 0){
+            for(int i=0; i<m; i++) matrix[i][0] = 0;
         }
-        for(int i=0; i<n; i++){
-            if(row == 0) matrix[0][i] = 0;
+        if(row == 0){
+            for(int i=0; i<n; i++){
+                matrix[0][i] = 0;
+            }
         }
     }
 };
