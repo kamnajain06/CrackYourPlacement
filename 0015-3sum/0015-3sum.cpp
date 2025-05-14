25092f575bf21980e7e978584caacf87dcf26354
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-14 23:25:16 +0530

    Time: 235 ms (19.41%), Space: 29.9 MB (40.92%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        
-        vector<vector<int>> v;
-        
-        for(int i=0; i<nums.size(); i++ ){
-            
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            vector<int> ans(3,-1);
-            ans[0] = nums[i];
-            
-            int left = i+1;
-            int right = nums.size()-1;
-            
-            while(left < right){
-                if(nums[left] + nums[right] + nums[i] == 0){
-                    cout<<nums[left]<<" "<<nums[right]<<endl;
-                    ans[1] = nums[left++];
-                    ans[2] = nums[right--];
-                    v.push_back(ans);
-                    
-                    while(left<right && nums[left] == nums[left-1]) left++;
-                    while(left<right && nums[right] == nums[right+1]) right--;
-                }else if(nums[left] + nums[right] + nums[i] > 0 ){
-                    right--;
-                }else{
-                    left++;
-                }
-                
-            }
+        unordered_map<int,int> mpp;
+
+        for(int i=0; i<n; i++){
+            mpp[nums[i]] = i;
         }
-        
-        return v;
+
+        for(int i=0; i<n; i++){
+            if(i >= 1 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j<n; j++){
+                if(j-1>i && nums[j] == nums[j-1]) continue;
+                int val = -(nums[i]+nums[j]);
+                if(mpp.find(val) != mpp.end() && mpp[val] > i && mpp[val] > j){
+                    ans.push_back({nums[i], nums[j], val});
+                }
+            }
+            
+        }
+        return ans;
     }
 };
