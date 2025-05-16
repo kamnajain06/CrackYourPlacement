938a5e8eb4f5b4073c43da2abef2aa9664957f5a
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-16 13:28:06 +0530

    Time: 59 ms (32.32%), Space: 29.1 MB (74.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -13,10 +13,12 @@ public:
                     ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }else if(nums[j] + nums[k]  + nums[i] > 0){
                     k--;
                 }else j++;
-                while(j < n && j-1 > i && nums[j] == nums[j-1]) j++;
+                
             }
             while(i < n-1 && nums[i] == nums[i+1]) i++;
         }
