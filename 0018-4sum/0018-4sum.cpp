2681a68bb48ca38c64bd8e62db2d102cc2feb4d8
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-16 13:37:34 +0530

    Time: 35 ms (20.23%), Space: 17.5 MB (31.17%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        unordered_map<int,int> mpp;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                while (j < n && j - 1 > i && nums[j] == nums[j - 1]) j++;
+                int left = j + 1;
+                int right = n - 1;
 
-        for(int i=0; i<n; i++){
-            mpp[nums[i]] = i;
-        }
-        for(int i=0; i<n; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j<n; j++){
-                if(j - 1 > i && nums[j] == nums[j-1]) continue;
-                for(int k=j+1; k<n; k++){
-                    if(k - 1 > j && nums[k] == nums[k-1]) continue;
-                    long long val = (long long) target-(long long)nums[i]-(long long)nums[j]-(long long) nums[k];
-                    if(val > INT_MAX || val < INT_MIN) continue;
-                    if(mpp.find(val) != mpp.end() && mpp[val] > i && mpp[val] > j && mpp[val] > k){
-                        cout<<mpp[val]<<endl;
-                        ans.push_back({nums[i], nums[j], nums[k], (int)val});
-                    }
+                while (left < right) {
+                    if ((long long) nums[i] + nums[j] + nums[left] + nums[right] ==
+                        target) {
+                        ans.push_back(
+                            {nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+                    } else if ((long long) nums[i] + nums[j] + nums[left] + nums[right] >
+                               target) {
+                        right--;
+                    } else
+                        left++;
                 }
             }
         }
