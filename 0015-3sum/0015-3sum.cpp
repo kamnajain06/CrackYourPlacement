b5a479155d087a70a7d3db81305a7193b5c964df
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-16 13:23:01 +0530

    Time: 55 ms (40.66%), Space: 29 MB (74.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        // T.c = o(n^2) , s.c = O(n)
-        vector<vector<int>> ans;
         int n = nums.size();
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        unordered_map<int,int> mpp;
 
         for(int i=0; i<n; i++){
-            mpp[nums[i]] = i;
-        }
-
-        for(int i=0; i<n; i++){
-            if(i >= 1 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j<n; j++){
-                if(j-1>i && nums[j] == nums[j-1]) continue;
-                int val = -(nums[i]+nums[j]);
-                if(mpp.find(val) != mpp.end() && mpp[val] > i && mpp[val] > j){
-                    ans.push_back({nums[i], nums[j], val});
-                }
+            int k = n-1;
+            int j = i+1;
+            while(j < k){
+                if(nums[j] + nums[k] + nums[i] == 0){
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }else if(nums[j] + nums[k]  + nums[i] > 0){
+                    k--;
+                }else j++;
+                while(j < n && j-1 > i && nums[j] == nums[j-1]) j++;
             }
-            
+            while(i < n-1 && nums[i] == nums[i+1]) i++;
         }
         return ans;
     }
