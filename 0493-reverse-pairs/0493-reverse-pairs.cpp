05b2c70f93a6e33d3d431920454a7baf3f3ddc0a
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-17 15:52:41 +0530

    Time: 381 ms (71.29%), Space: 147 MB (72.52%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,61 +1,47 @@
 class Solution {
 public:
-void mergeSort(vector<int>& nums,long long start, long long end,long long &count){
-        long long mid = (start+end)/2;
-        long long  startIndex = start;
-        long long n1= mid-start+1;
-        long long n2= end - mid;
-        vector<int> arr1(n1);
-        vector<int> arr2(n2);
-        for(long long i=0; i<n1; i++){
-            arr1[i]=nums[startIndex++];
-        }
-        for(long long i=0; i<n2; i++){
-            arr2[i]=nums[startIndex++];
-        }
-        long long i=0;
-        long long j=0;
-        while(i<n1 && j<n2){
-            if((long long)arr1[i]>(long long)2*arr2[j]){
+    void merge(vector<int> &nums, int start, int end, int &cnt){
+        int mid = (start + end)/2;
+        int n1 = mid - start + 1, n2 = end - mid;
+        vector<int> arr1(n1), arr2(n2);
+        int idx = start;
+        for(int i=0; i<n1; i++) arr1[i] = nums[idx++];
+        for(int i=0; i<n2; i++) arr2[i] = nums[idx++];
+
+        int i = 0, j = 0;
+        idx = start;
+        while(i < n1 && j < n2){
+            if(arr1[i] > (long long)2* arr2[j]){
+                cnt += n1-i;
                 j++;
             }else{
-                count+=j;
-                i++;
+               i++;
             }
         }
-        while(i<n1){
-            count+=j;
-            i++;
-        }
-        i=0;
-        j=0;
-        while(i<n1 && j<n2){
-            if(arr1[i]<=arr2[j]){
-                nums[start++]=arr1[i++];
+        i=0, j=0;
+        while(i < n1 && j < n2){
+            if(arr1[i] <= arr2[j]){
+                nums[idx++] = arr1[i++];
             }else{
-                nums[start++]=arr2[j++];
+                nums[idx++] = arr2[j++];
             }
-            
         }
-        while(i<n1){
-            nums[start++]=arr1[i++];
-        }
-        while(j<n2){
-            nums[start++]=arr2[j++];
-        }
+        while(i < n1) nums[idx++] = arr1[i++];
+        while(j < n2) nums[idx++] = arr2[j++];
+
     }
-    void merge(vector<int>& nums,long long start,  long long end, long long &count){
-        if(start>=end){
-            return;
-        }
-        long long mid = (start+end)/2;
-        merge(nums,start,mid,count);
-        merge(nums,mid+1,end,count);
-        mergeSort(nums,start,end,count);
+    void mergeSort(vector<int> &nums, int start, int end, int &cnt){
+        if(start >= end) return;
+        int mid = (start + end)/2;
+
+        mergeSort(nums, start, mid, cnt);
+        mergeSort(nums, mid+1, end, cnt);
+        merge(nums, start, end, cnt);
     }
     int reversePairs(vector<int>& nums) {
-        long long count = 0;
-        merge(nums,0,nums.size()-1,count);
-        return (int)count;
+        int n = nums.size();
+        int cnt = 0;
+        mergeSort(nums, 0, n-1, cnt);
+        return cnt;
     }
 };
