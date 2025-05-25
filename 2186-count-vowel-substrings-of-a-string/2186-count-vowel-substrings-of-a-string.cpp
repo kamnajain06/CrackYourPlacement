eb55a4a0ce9fe544cd65a76f4bd59647e0e123d3
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-25 20:49:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.35%) - LeetHub

---
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countVowelSubstrings(string word) {
+        int n = word.length();
+        vector<pair<int, int>> v;
+        int start = -1, end = -1;
+        int i = 0;
+        for (; i < n; i++) {
+            if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' ||
+                word[i] == 'o' || word[i] == 'u') {
+                if (start == -1)
+                    start =  end = i;
+                else
+                    end = i;
+            } else {
+                if (start != -1 && end != -1) {
+                    v.push_back({start, end});
+                }
+                start = -1;
+                end = -1;
+            }
+        }
+        if (start != -1) {
+            v.push_back({start, end});
+        }
+        int cnt = 0;
+        for(auto it : v){
+            i = it.first;
+            int end = it.second;
+            unordered_map<char,int> mpp;
+            for(int j = i; j<=end; j++){
+                mpp[word[j]]++;
+                while(mpp.size() >= 5){
+                    cnt += end - j+1;
+                    mpp[word[i]]--;
+                    if(mpp[word[i]] == 0) mpp.erase(word[i]);
+                    i++;
+                }
+            }
+            cout<<i<<","<<end<<endl;
+        }
+        return cnt;
+    }
+};
