656e0f9c870d5918fe72276a6d23d25c24115be5
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-27 01:47:59 +0530

    Time: 0 ms (100%), Space: 10 MB (89.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int fun(int n, int sum, vector<int> &nums, vector<int> &dp){
-        if(n >= nums.size()){
-            return 0;
-        }
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = max(nums[n] + fun(n+2, sum , nums, dp), fun(n+1, sum, nums, dp));
-    }
+    // int f(int i, vector<int> &nums, int n, vector<int> &dp){
+    //     if(i >= n) return 0;
+
+    //     if(dp[i] != -1) return dp[i];
+
+    //     int take = nums[i] + f(i+2, nums, n, dp);
+    //     int skip = f(i+1, nums, n, dp);
+
+    //     return dp[i] = max(take, skip);
+    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // // vector<int> dp(n+1, -1);
-        // dp[0] = nums[0];
-        int prev2 = 0;
-        int prev = nums[0];
-        for(int i=1; i<n; i++){
-            int pick = i-2 >= 0 ? nums[i] + prev2 : nums[i];
-            int curri = max(pick, prev);
-            prev2 = prev;
-            prev = curri;
+        // vector<int> dp(n, -1);
+        // return f(0, nums, n, dp);
+
+        // vector<int> dp(n, 0);
+        int prev1 = 0, prev2 = 0;
+        for(int i=n-1; i>=0; i--){
+            int take = i + 2 < n ? nums[i] + prev2 : nums[i];
+            int skip = i + 1 < n ? prev1 : 0;
+
+            int curr = max(take, skip);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return prev;
+
+        return prev1;
     }
 };
