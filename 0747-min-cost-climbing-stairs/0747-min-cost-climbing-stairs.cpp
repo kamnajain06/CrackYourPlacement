bf4c85aab3caeaed95cd791251ce67402563fcca
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-26 20:00:37 +0530

    Time: 0 ms (100%), Space: 17.7 MB (29.11%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // int f(int ind, int n , vector<int> &cost){
+    //     if(ind >= n) return 0;
+    //     return cost[ind] + min(f(ind + 1, n, cost), f(ind + 2, n, cost));
+    // }
+
+    // int f(int ind, int n , vector<int> &cost, vector<int> &dp){
+    //     if(ind >= n) return 0;
+    //     if(dp[ind] != -1) return dp[ind];
+    //     return dp[ind] = cost[ind] + min(f(ind + 1, n, cost, dp), f(ind + 2, n, cost, dp));
+    // }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        // < recursion >
+        // if(n == 2) return min(cost[0], cost[1]);
+        // return min(f(0, n, cost), f(1, n, cost));
+
+        // < memo >
+        // vector<int> dp(n+1, -1);
+        // if(n == 2) return min(cost[0], cost[1]);
+        // return min(f(0, n, cost, dp), f(1, n, cost, dp));
+
+        // < Tab >
+        vector<int> dp(n+1, 0);
+        dp[n] = 0;
+        dp[n-1] = 0;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = min(cost[i-1] + dp[i-1] , cost[i-2] + dp[i-2]);
+        }
+        return dp[n];
+
+        
+    }
+};
