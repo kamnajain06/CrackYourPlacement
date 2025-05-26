5315affbc227f957a0c0fdc7a0d4eec07d32e2b7
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-26 20:01:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (51.54%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -24,8 +24,6 @@ public:
 
         // < Tab >
         vector<int> dp(n+1, 0);
-        dp[n] = 0;
-        dp[n-1] = 0;
 
         for(int i=2; i<=n; i++){
             dp[i] = min(cost[i-1] + dp[i-1] , cost[i-2] + dp[i-2]);
