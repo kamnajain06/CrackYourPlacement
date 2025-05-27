5d71922db95cf527ed1b3c55227f75147133c103
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-27 19:37:29 +0530

    Time: 183 ms (21.62%), Space: 216.5 MB (26.1%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,25 +1,68 @@
 class Solution {
 public:
-    bool f(int i, int jump, vector<int> &stones, int n, unordered_map<int, int> &mpp, vector<vector<int>> &dp){
-        if(i >= n-1) return true;
+    // bool f(int i, int jump, vector<int>& stones, int n,
+    //        unordered_map<int, int>& mpp, vector<vector<int>>& dp) {
+    //     if (i >= n - 1)
+    //         return true;
 
-        if(dp[i][jump] != -1) return dp[i][jump];
-        bool val = false;
+    //     if (dp[i][jump] != -1)
+    //         return dp[i][jump];
+    //     bool val = false;
 
-        bool op1 = mpp.find(stones[i] + jump-1) != mpp.end() && mpp[stones[i] + jump-1] > i? f(mpp[stones[i] + jump - 1], jump-1, stones, n, mpp, dp) : false;
-        bool op2 = mpp.find(stones[i] + jump) != mpp.end() && mpp[stones[i] + jump] > i ? f(mpp[stones[i] + jump], jump, stones, n, mpp, dp) : false;
-        bool op3 = mpp.find(stones[i] + jump+1) != mpp.end() && mpp[stones[i] + jump+1] > i? f(mpp[stones[i] + jump + 1], jump+1, stones, n, mpp, dp) : false;
-        
-        val = op1 || op2 || op3;
+    //     bool op1 =
+    //         mpp.find(stones[i] + jump - 1) != mpp.end() &&
+    //                 mpp[stones[i] + jump - 1] > i
+    //             ? f(mpp[stones[i] + jump - 1], jump - 1, stones, n, mpp, dp)
+    //             : false;
+    //     bool op2 =
+    //         mpp.find(stones[i] + jump) != mpp.end() && mpp[stones[i] + jump] > i
+    //             ? f(mpp[stones[i] + jump], jump, stones, n, mpp, dp)
+    //             : false;
+    //     bool op3 =
+    //         mpp.find(stones[i] + jump + 1) != mpp.end() &&
+    //                 mpp[stones[i] + jump + 1] > i
+    //             ? f(mpp[stones[i] + jump + 1], jump + 1, stones, n, mpp, dp)
+    //             : false;
 
-        return dp[i][jump] = val;
-    }
+    //     val = op1 || op2 || op3;
+
+    //     return dp[i][jump] = val;
+    // }
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-        if(stones[1] != 1) return false;
+
+        // if(stones[1] != 1) return false;
+        // unordered_map<int, int> mpp;
+        // vector<vector<int>> dp(n, vector<int> (n, -1));
+        // for(int i=0; i<n; i++) mpp[stones[i]] = i;
+
+        // return f(1, 1, stones, n, mpp, dp);
+
+        if (stones[1] != 1)
+            return false;
         unordered_map<int, int> mpp;
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        for(int i=0; i<n; i++) mpp[stones[i]] = i;
-        return f(1, 1, stones, n, mpp, dp);
+        vector<vector<int>> dp(n, vector<int>(n, false));
+        for (int i = 0; i < n; i++)
+            mpp[stones[i]] = i;
+        
+        dp[0][0] = true;
+
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                if(!dp[i][j]) continue;
+                for(int k = -1; k <= 1; k++){
+                    if(j + k >= 0){
+                        if(mpp.find(stones[i] + j + k) != mpp.end()){
+                            dp[mpp[stones[i] + j + k]][j+k] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int j=0; j<n; j++){
+            if(dp[n-1][j] == 1) return true;
+        }
+        return false;
     }
 };
