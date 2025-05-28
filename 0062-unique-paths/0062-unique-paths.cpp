ec1c8a18d2b11acded090bb03ec2ee5e4e365b39
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-05-29 04:32:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (74.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    // int dfs(int i, int j, int m , int n, vector<vector<int>> &dp){
-    //     if(i == 0 && j == 0) return 1;
-    //     if(i < 0 || j < 0) return 0;
+    // int f(int i, int j, int m , int n, vector<vector<int>> &dp){
+    //     if(i >= m || j >= n) return 0;
+    //     if(i == m-1 && j == n-1) return 1;
     //     if(dp[i][j] != -1) return dp[i][j];
-    //     int right = dfs(i, j-1, m, n, dp);
-    //     int down = dfs(i-1, j, m, n, dp);
-    //     return dp[i][j] = right + down;
+
+    //     return dp[i][j] = f(i+1, j, m, n, dp) + f(i, j+1, m, n, dp);
     // }
     int uniquePaths(int m, int n) {
-        // Memoization
         // vector<vector<int>> dp(m, vector<int> (n, -1));
-        // return dfs(m-1, n-1, m, n, dp);
+        // return f(0, 0, m, n, dp);
 
-        // // Tabulation
-        // vector<vector<int>> dp(m, vector<int> (n, -1));
-        // for(int i=0; i<m; i++){
-        //     for(int j=0; j<n; j++){
-        //         if(i == 0 && j == 0) dp[i][j] = 0;
-        //         else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        // vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+
+        // for(int i = m-1; i>=0; i--){
+        //     for(int j = n-1; j>=0; j--){
+        //         if(i == m-1 && j == n-1) dp[m-1][n-1] = 1;
+        //         else{
+        //             dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        //         }
         //     }
         // }
-        // return dp[m-1][n-1];
+        // return dp[0][0];
 
-        // Space Optimization
-        vector<int> dp(n, 0);
-        dp[0] = 1;
-        for(int i=0; i<m; i++){
-            vector<int> temp(n,0);
-            for(int j=0; j<n; j++){
-                int left = j-1 >= 0 ? temp[j-1] : 0;
-                temp[j] = left + dp[j];
+        vector<int> dp(n+1, 1);
+        for(int i = m-2; i>=0; i--){
+            vector<int> curr(n+1, 0);
+            for(int j = n-1; j>=0; j--){
+                curr[j] = dp[j] + curr[j+1];
             }
-            dp = temp;
+            dp = curr;
         }
-        return dp[n-1];
+        return dp[0];
     }
 };
