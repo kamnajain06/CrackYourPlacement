fdcdff4414e04c818956f415093a1b6dca5179fe
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-06-01 03:16:17 +0530

    Time: 75 ms (9.13%), Space: 48.8 MB (41.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-//     int fun(int amount, vector<int> &coins, int n, vector<int> &dp){
-//         if(amount == 0) return 0;
+    // int f(int i, vector<int> &coins, int amount){
+    //     if(i < 0){
+    //         return amount == 0 ? 0 : 1e9;
+    //     }
         
-//         if(dp[amount] != -1) return dp[amount];
+    //     int skip = f(i-1, coins, amount);
+    //     int take = coins[i] <= amount ? 1 + f(i, coins, amount - coins[i]) : 1e9;
 
-//         int mini = 1e8;
-//         for(int i=0; i<n; i++){
-//             int pick = coins[i] <= amount ? 1 + fun(amount-coins[i], coins, n, dp) : 1e8;
-//             mini = min(mini, pick);
-//         }
-        
-//         return dp[amount] = mini;
-//     }
+    //     return min(skip, take);
+    // }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<int> dp(amount+1, -1);
-        // int res = fun(amount, coins, n, dp);
-        // return res == 1e8 ? -1 : res;
-        
-        // TABULATION
-        vector<int> dp(amount+1, 0);
-        for(int it = 1; it<= amount; it++){
-            int mini = 1e8;
-            for(int i=0; i<n; i++){
-                int pick = coins[i] <= it ? 1 + dp[it-coins[i]] : 1e8;
-                mini = min(mini, pick);
+        // int ans = f(n-1, coins, amount);
+        // return ans == 1e9 ? -1 : ans;
+
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= amount; j++){
+                int skip = i-1 >= 0 ? dp[i-1][j] : j == 0 ? 0 : 1e9;
+                int take = coins[i] <= j ? 1 + dp[i][j - coins[i]] : 1e9;
+
+                dp[i][j] = min(skip, take);
+                // cout << dp[i][j] << " " ;
             }
-            dp[it] = mini;
+            // cout << endl;
         }
-        return dp[amount] == 1e8 ? -1 : dp[amount];
+
+        return dp[n-1][amount] == 1e9 ? -1 : dp[n-1][amount];
+
+
     }
 };
