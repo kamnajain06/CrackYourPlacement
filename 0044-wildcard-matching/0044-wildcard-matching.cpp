e88dc1524b54d385d39221f960e7c1e4438ebcdb
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-06-04 19:48:17 +0530

    Time: 35 ms (43.91%), Space: 14.9 MB (53.8%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,32 +1,58 @@
 class Solution {
 public:
-    bool f(int i, int j, int n1, int n2, string &s, string &p, vector<vector<int>> &dp){
-        if(i == n1){
-            while(j != n2){
-                if(p[j++] != '*') return false;
-            }
-            return true;
-        }
-        if(i == n1 && j == n2) return true;
-        if(i >= n1 || j >= n2) return false;
+    // bool f(int i, int j, int n1, int n2, string& s, string& p,
+    //        vector<vector<int>>& dp) {
+    //     if (i == n1) {
+    //         while (j != n2) {
+    //             if (p[j++] != '*')
+    //                 return false;
+    //         }
+    //         return true;
+    //     }
+    //     if (i == n1 && j == n2)
+    //         return true;
+    //     if (i >= n1 || j >= n2)
+    //         return false;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
 
-        bool val = false;
+    //     bool val = false;
 
-        if(s[i] == p[j] || p[j] == '?'){
-            val = val || f(i+1, j+1, n1, n2, s, p, dp);
-        }
-        else if(p[j] == '*'){
-            val = val || f(i+1, j, n1, n2, s, p, dp) || f(i+1, j+1, n1, n2, s, p, dp) || f(i, j+1, n1, n2, s, p, dp);
-        }
-        return dp[i][j] = val;
-    }
+    //     if (s[i] == p[j] || p[j] == '?') {
+    //         val = val || f(i + 1, j + 1, n1, n2, s, p, dp);
+    //     } else if (p[j] == '*') {
+    //         val = val || f(i + 1, j, n1, n2, s, p, dp) ||
+    //               f(i + 1, j + 1, n1, n2, s, p, dp) ||
+    //               f(i, j + 1, n1, n2, s, p, dp);
+    //     }
+    //     return dp[i][j] = val;
+    // }
     bool isMatch(string s, string p) {
         int n1 = s.length();
         int n2 = p.length();
         // if(n1 == 0) return true;
-        vector<vector<int>> dp(n1, vector<int> (n2, -1));
-        return f(0, 0, n1, n2, s, p, dp);
+        // vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        // return f(0, 0, n1, n2, s, p, dp);
+
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[n1][n2] = true;
+        for(int j = n2-1; j >=0; j--){
+            if (p[j] == '*'){
+                dp[n1][j] = true;
+            }else break;
+        }
+        for (int i = n1-1; i >= 0; i--) {
+            for (int j = n2-1; j >= 0; j--) {
+                bool val = false;
+                if (s[i] == p[j] || p[j] == '?') {
+                    val = val || dp[i + 1][j + 1];
+                } else if (p[j] == '*') {
+                    val = val || dp[i + 1][j] || dp[i + 1][j + 1] || dp[i][j + 1];
+                }
+                dp[i][j] = val;
+            }
+        }
+        return dp[0][0];
     }
 };
