63a49e77e7d47b25e51db074b6a55ce7c98cec11
Kamna Jain <151832026+kamnajain06@users.noreply.github.com>
2025-06-11 12:13:47 +0530

    Time: 15 ms (32.08%), Space: 12.6 MB (49.76%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    // int fun(int i, vector<int> &nums, int n, int prev){
-    //     if(i == n) return 0;
-
-    //     if(nums[i] % nums[prev] == 0 || nums[prev] % nums[i] == 0){
-    //         return max(1 + fun(i+1, nums, n, i), fun(i+1, nums, n, prev));
-    //     }
-    //     return fun(i+1, nums, n, i);
-    // }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
-        // int val = fun(1, nums, n, 0) + 1;
         sort(nums.begin(), nums.end());
-        vector<int> dp(n, 1);
-        vector<int> backtrack(n);
-        for(int i=0; i<n; i++) backtrack[i] = i;
-        int maxLen = 1;
-        int ind = 0;
-        for(int i = 0; i<n; i++){
-            for(int prev = 0; prev < i; prev++){
-                if(nums[i] % nums[prev] == 0 && dp[i] < 1 + dp[prev]){
-                    dp[i] = 1 + dp[prev];
-                    backtrack[i] = prev;
+        vector<int> dp(n, 1), backtrack(n, 0);
+        iota(backtrack.begin(), backtrack.end(), 0);
+        int maxInd = 0;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j<i; j++){
+                if(nums[i]%nums[j] == 0){
+                    if(dp[i] < 1 + dp[j]){
+                        dp[i] = 1 + dp[j];
+                        backtrack[i] = j;
+                        if(dp[i] > dp[maxInd]) maxInd = i;
+                    }
                 }
             }
-            maxLen = max(maxLen, dp[i]); 
-            if(dp[i] > dp[ind]) ind = i;
         }
-        
-        vector<int> ans(maxLen);
-        ans[0] = nums[ind]; 
-        int i = 1;
+        int ind = maxInd;
+        vector<int> v;
         while(backtrack[ind] != ind){
-            ans[i] = nums[backtrack[ind]];
+            v.push_back(nums[ind]);
             ind = backtrack[ind];
-            i++;
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        v.push_back(nums[ind]);
+        reverse(v.begin(), v.end());
+        return v;
+
     }
 };
